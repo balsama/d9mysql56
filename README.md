13724e19cf4a9a922e16003872bdfac147c80d69
Adam Balsam <adam.balsam@acquia.com>
2020-03-18 22:46:55 -0400

    D8 starting point

---
+++ README.md
@@ -0,0 +1,3 @@
+# Drupal 9 with MySQL 5.6
+Demo project that shows the minimum modifications needed to run a Drupal 9 site
+with MySQL 5.6.
---
+++ hooks/common/post-code-update/post-code-update.sh
@@ -0,0 +1,15 @@
+  #!/bin/sh
+#
+# Cloud Hook: Rebuild the cache and run any pending database updates.
+#
+# Run `drush cache-rebuild` and `drush updatedb` in the target environment.
+
+site="$1"
+target_env="$2"
+
+/usr/local/bin/drush9 --version
+/usr/local/bin/drush9 @$site.$target_env cache-rebuild --yes
+/usr/local/bin/drush9 @$site.$target_env updatedb --yes
+/usr/local/bin/drush9 @$site.$target_env cache-rebuild --yes
+/usr/local/bin/drush9 @$site.$target_env config-import --no-interaction
+/usr/local/bin/drush9 @$site.$target_env cache-rebuild --yes
